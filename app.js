b813d879d04a4ae48c795bfe297a0f19d1748401
Cameron Sanchez <csanchez2237@gmail.com>
2017-11-02 15:56:30 -0600

    basic setup

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 'use strict';
 
 const express = require('express');
+const favicon = require('serve-favicon');
 const fetch = require('node-fetch');
 const path = require('path');
 const pug = require('pug');
+const swearjar = require('swearjar');
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 const apiBaseUrl = 'http://api.flutrack.org/';
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   if (req.query.q) {
-    fetch(`${apiBaseUrl}?s=${req.query.q}`)
+    fetch(`${apiBaseUrl}?s=${req.query.q}&time=7`)
       .then((response) => response.json())
       .then((tweets) => res.render('index.pug', {
-        tweets
-      }));
+        listTitle: `Total reports of ${req.query.q}:`,
+        tweets: tweets.map((tweet => swearjar.censor(tweet.tweet_text)))
+      }))
+      .catch(next);
   } else {
     fetch(`${apiBaseUrl}?time=7`)
       .then((response) => response.json())
       .then((tweets) => res.render('index.pug', {
-        tweets
-      }));
+        listTitle: 'Total reports of flu-like symptoms over the last 7 days:',
+        tweets: tweets.map((tweet => swearjar.censor(tweet.tweet_text)))
+      }))
+      .catch(next);
   }
 });
 
-app.listen('3000');
+app.use((req, res) => res.status(404).render('404.pug'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('500.pug');
+});
+
+app.listen(3007);
--- public/index.js
+++ public/index.js
@@ -1,3 +1,14 @@
-document.querySelector('#submit').addEventListener('click', function () {
-  window.location = '/?q=' + document.querySelector('#search').value;
+'use strict';
+
+const searchBar = document.querySelector('#searchBar');
+const searchButton = document.querySelector('#searchButton');
+
+searchBar.addEventListener('keyup', function (key) {
+  if (key.which === 13 || key.keyCode === 13) {
+    searchButton.click();
+  }
+});
+
+searchButton.addEventListener('click', function () {
+  window.location = '/?q=' + searchBar.value;
 });
