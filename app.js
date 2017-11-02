962ad724a21e0e75437d98d94ab7287c2699f017
Cameron Sanchez <csanchez2237@gmail.com>
2017-11-02 11:18:25 -0600

    set up pug

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const express = require('express');
+const pug = require('pug');
+const request = require('request');
 const app = express();
 
+app.set('view engine', 'pug');
+
+const apiBaseUrl = 'http://api.flutrack.org/';
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  // if (req.query.q) {
+  //   request.get(`${apiBaseUrl}?s=${req.query.q}`).pipe(res);
+  // } else {
+  //   request.get(`${apiBaseUrl}?time=7`).pipe(res);
+  // }
+  res.render('index.pug', {
+    title: 'Hello, World!'
+  })
 });
 
 app.listen('3000');
